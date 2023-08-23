a3e341890f60c9a865e20ed3d5636d84fc27a8fb
aaronlxsell <aaron.lau@xselltechnologies.com>
2023-08-23 09:59:10 -0500

    update task for tako
    Added functionality to task and extended component. Next step is to render it on the page and add CSS

--- aaron-lau-website/src/Containers/Tako/Scenes/Task.jsx
+++ aaron-lau-website/src/Containers/Tako/Scenes/Task.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { useState } from 'react';
 import './Task.scss'
 
+const Priority = Enum({
+  Highest: 5,
+  High: 4,
+  Medium: 3,
+  Low: 2,
+  Lowest: 1
+})
 
-const Task = () => {
-  const [title, setTitle] = useState(0);
-  const [description, setDescription] = useState(0);
-  return (
-    <div className='task'>
-      <div className='title-border'>
-        <input name="title"></input>
-        <div className='priority'></div>
-      </div>
-      <div className='description-border'>
-        <p className='description'>Write about your task here...</p>
-      </div>
-      <div className='status'>Backlog</div>
-      <div className='linked-task-type'></div>
-      <div className='linked-task'></div>
+const Status = Enum({
+  Backlog: 'BACKLOG',
+  ToDo: 'TO DO',
+  InProgress: 'IN PROGRESS',
+  Blocked: 'BLOCKED',
+  Review: 'READY FOR REVIEW',
+  Done: 'DONE'
+})
 
-    </div>
-  )
+export default class Task extends React.Component {
+  constructor(Title = 'New task...', prior = Priority.Medium, Description = '', stat = Status.Backlog) {
+    this.title = Title
+    this.priority = prior
+    this.description = Description
+    this.status = stat
+  }
+  
+
+  render(){
+    return (
+      <div className='task'>
+        <div className='header'>
+        <nav className='title'>{this.props.Title}</nav>
+        <nav className='priority'>{this.props.Priority}</nav>
+        </div>
+        <div className='body'>
+          <button className='status'>{this.props.Status}</button>
+          <div>
+            <p className='description'>{this.props.description}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default Task
