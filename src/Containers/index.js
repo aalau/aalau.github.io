1771df9eadc0b3a6f915bb58cc9b3c62d9316fff
Aaron Lau <aaronlau12@gmail.com>
2023-12-28 12:29:34 -0600

    Revert "Updates"
    This reverts commit 203d1758045f5b4562a7ad241feefcb7362eb170.

---
+++ src/Containers/index.js
@@ -0,0 +1,12 @@
+export {default as About } from './about/About';
+export {default as Blog } from './blog/Blog';
+export {default as BlogFull} from './blog/BlogFull';
+export {default as Features } from './features/Features';
+export {default as Footer } from './footer/Footer';
+export {default as Header } from './header/Header';
+export {default as Projects } from './projects/Projects';
+export {default as ProjectsFull } from './projects/ProjectsFull';
+export {default as Resume } from './resume/Resume';
+export {default as Tako } from './Tako/Tako';
+export {default as TakoFull} from './Tako/TakoFull';
+export {default as ErrorBoundary} from './ErrorBoundary/ErrorBoundary';
